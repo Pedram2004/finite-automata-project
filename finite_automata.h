57f2b3bd7a0dd77d2587f4c4d1dbde836f76da05
Pedram Alizadeh <83alizadeh13ped@gmail.com>
2024-12-28 20:17:10 +0330

    FiniteAutomata Constructor created and class completed

--- finite_automata.h
+++ finite_automata.h
@@ -7,13 +7,22 @@
 class FiniteAutomata {
 protected:
     int initial_state;
-    int alphabet_num;
+    int alphabet_number;
     std::vector<int> final_states;
     std::map<int, std::vector<int>> transition_graph;
+
+    FiniteAutomata(int initial_state, int alphabet_num, std::vector<int> &final_states,
+                   std::map<int, std::vector<int>> &transition_graph) {
+        this->initial_state = initial_state;
+        this->alphabet_number = alphabet_num;
+        this->final_states = std::vector<int>(final_states);
+        this->transition_graph = std::map<int, std::vector<int>> (transition_graph);
+    }
 
     int transition_function(int current_state, int alphabet_letter) {
         return this->transition_graph[current_state][alphabet_letter];
     }
 };
+
 
 #endif
