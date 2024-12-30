6673ea3c29e7fcb54524d4978a7333ea96d4b685
Pedram Alizadeh <83alizadeh13ped@gmail.com>
2024-12-30 18:49:12 +0330

    class template FiniteAutomata created with its related attributes and functions (i.e. is_state_final and transition_function)

--- finite_automata.h
+++ finite_automata.h
@@ -1,33 +1,35 @@
 #ifndef DFA_FINITE_AUTOMATA_H
 #define DFA_FINITE_AUTOMATA_H
 
-#include <map>
 #include <vector>
-#include <algorithm>
+#include <set>
+#include <map>
 
+template<typename T>
 class FiniteAutomata {
+    /* NFA and DFA parent class
+     * must include a transition function for both of them (including epsilon moves of NFA) */
 protected:
-    int initial_state;
-    int alphabet_number;
-    std::vector<int> final_states;
-    std::map<int, std::vector<int>> transition_graph;
+    int initial_state{}, alphabet_number{};
+    std::set<int> final_states;
+    std::map<int, std::vector<T>> transition_graph;
 
-    FiniteAutomata(int initial_state, int alphabet_num, std::vector<int> &final_states,
-                   std::map<int, std::vector<int>> &transition_graph) {
-        this->initial_state = initial_state;
-        this->alphabet_number = alphabet_num;
-        this->final_states = std::vector<int>(final_states);
-        this->transition_graph = std::map<int, std::vector<int>>(transition_graph);
-    }
+    FiniteAutomata(int _init_state, int _alphabet_number, std::set<int> &_final_states,
+                   std::map<int, std::vector<T>> &_transition_graph) {
+        this->initial_state = _init_state;
+        this->alphabet_number = _alphabet_number;
+        this->final_states = std::set<int>(_final_states);
+        this->transition_graph = std::map<int, std::vector<T>> (_transition_graph);
 
-    int transition_function(int current_state, int alphabet_letter) {
-        return this->transition_graph[current_state][alphabet_letter];
     }
 
     bool is_state_final(int _state) {
-        return std::find(this->final_states.begin(), this->final_states.end(), _state) != this->final_states.end();
+        return this->final_states.count(_state);
+    }
+
+    T transition_function(int _current_state, int _alphabet_letter) {
+        return this->transition_graph[_current_state][_alphabet_letter];
     }
 };
-
 
 #endif
